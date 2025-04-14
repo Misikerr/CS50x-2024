9821143886216ae7285da47add2e299e80b37569
Misiker_Genene <146325517+Misikerr@users.noreply.github.com>
2025-04-14 11:51:46 +0300

    Add files via upload

---
+++ me/hello.c
@@ -0,0 +1,48 @@
+
+#include <cs50.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, string argv[])
+{
+    if (argc != 2)
+    {
+        printf("Usage: ./caesar key\n");
+        return 1;
+    }
+
+    for (int i = 0; i < strlen(argv[1]); i++)
+    {
+        if (!isdigit(argv[1][i]))
+        {
+            printf("Usage: ./caesar key\n");
+            return 1;
+        }
+    }
+
+    int k = atoi(argv[1]);
+
+
+    string plaintext = get_string ("Plaintext: ");
+
+    printf("Cipertext: ");
+
+    for (int j = 0; j < strlen(plaintext); j++)
+    {
+        if(isupper(plaintext[j]))
+        {
+            printf("%c", (plaintext[j] - 'A' + k) % 26 + 'A');
+        }
+         if(islower(plaintext[j]))
+        {
+            printf("%c", (plaintext[j] - 'a' + k) % 26 + 'a');
+        }
+        else
+        {
+            printf("%c", plaintext[j]);
+        }
+    }
+    printf("\n");
+}
