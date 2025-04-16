88cb67bd52fc9f98220b2923ed3c831ad3c8a5fa
Misiker_Genene <146325517+Misikerr@users.noreply.github.com>
2025-04-16 18:02:58 +0300

    Update mario.c

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -1,20 +1,36 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void){
+int main(void)
+{
     int height;
-    do{
-        height = get_int("Enter hight here:");
-    }
-    while(height < 1 || height > 8);
 
-    for(int row = 0; row < height; row++){
-        for(int space = 0; space < height - row - 1;space++){
+    // Prompt the user for a height between 1 and 8 (inclusive)
+    do
+    {
+        height = get_int("Enter height here: ");
+    }
+    while (height < 1 || height > 8);
+
+    // Loop through each row
+    for (int row = 0; row < height; row++)
+    {
+        // Print spaces to align the pyramid to the right
+        for (int space = 0; space < height - row - 1; space++)
+        {
             printf(" ");
         }
-        for(int column = 0; column <= row; column++){
+
+        // Print left-aligned hashes
+        for (int column = 0; column <= row; column++)
+        {
             printf("#");
         }
+
+        // Print the fixed extra hash to form the right side of the pyramid
         printf("#");
+
+        // Move to the next line after each row
+        printf("\n");
     }
 }
